2d22314fd1bee08536b8b71b49c0284f14ca0f2d
Hawkeye Roe <r.roe@live.co.uk>
2016-03-16 10:55:20 +0000

    Need to check any moves valid

--- Frog and toad.py
+++ Frog and toad.py
@@ -21,25 +21,40 @@ def main():
 def menu():
     choice = 0
     print("""\n\tWelcome to Frog's vs toads.\n
-Pick an option:
-\t1. Play game
-\t2. Quit""")
-    while(choice ==0):
-        try:
-            choice = int(input("\nEnter your choice: "))
-        except ValueError:
-            print("Value error. Please enter a valid choice")
-    return choice
-
+    Pick an option:
+    \t1. Play game
+    \t2. Quit""")
+        while(choice ==0):
+            try:
+                choice = int(input("\nEnter your choice: "))
+            except ValueError:
+                print("Value error. Please enter a valid choice")
+        return choice
 
 def PlayGame():##Entire game is run from this function
-    GameList = ["T", "T", "T", "", "F", "F", "F"] ##Initialises "log" in game
+    OriginalLog = ["T", "T", "T", "", "F", "F", "F"] ##Initialises "log" in game
+    GameList = OriginalLog
     MovesValid = True
     while(MovesValid):
         print("Here is your log: " + str(GameList))
-        GameList = MoveFrog(GameList)
+        choice = PlayerOption()
+        if(choice == 1):
+            GameList = MoveFrog(GameList)
+        if(choice == 2):
+            GameList = OriginalLog
+            print("Log reset")
+        if(choice == 3):
+            print("Goodbye")
+            sys.exit()
+    if(GameList == OriginalLog[::-1]):
+        print("CONGRATULATIONS, YOU'VE WON")
+    else:
+        print("No moves left. Bad Luck")
         
-        
+def PlayerOption():
+    print("\n\tWhat will you do?\n1. Move Frogs\n2. Reset log\n3. Quit program")
+    choice = int(input("\nEnter Choice: "))
+    return choice
 
 def MoveFrog(GameList):
     n1 = int(input("Select frog to move: "))
@@ -69,8 +84,17 @@ def CheckValidMove(n1, n2, GameList): ##Checks Reasons that move could not be co
         print("Target space is not free")
         IsMoveValid = False
         return IsMoveValid
+    if(GameList[n1] == "T" and n2<n1):
+        print("Toads cannot move backwards")
+        IsMoveValid = False
+        return IsMoveValid
+    if((GameList[n1] == "F" and n1<n2)):
+        print("Frogs cannot move backwards")
+        IsMoveValid = False
+        return IsMoveValid
     return IsMoveValid
     
 def MoveFrogPositions(OriginalPos, NewPos, GameList):
-    print("Move Frogs")
+    GameList[NewPos] = GameList[OriginalPos]
+    GameList[OriginalPos] = ""
     return GameList
