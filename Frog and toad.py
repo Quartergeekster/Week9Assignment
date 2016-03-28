54ff227666c414e9f6d4926598f893428daac584
= <r.roe@live.co.uk>
2016-03-28 13:03:21 +0100

    Game Complete

--- Frog and toad.py
+++ Frog and toad.py
@@ -53,6 +53,8 @@ def PlayGame():##Entire game is run from this function
         if(choice == 3):
             print("Goodbye")
             sys.exit()
+        else:
+            print("Choice not valid")
         MovesValid = AnyMovesValid(GameList)
     if(GameList == OriginalLog[::-1]):
         print(GameList)
@@ -61,11 +63,17 @@ def PlayGame():##Entire game is run from this function
         print("No moves left. Bad Luck")
         
 def PlayerOption():
+    choice = 0
     print("\n\tWhat will you do?\n1. Move Frogs\n2. Reset log\n3. Quit program")
-    choice = int(input("\nEnter Choice: "))
+    while(choice == 0):
+        try:
+            choice = int(input("\nEnter Choice: "))
+        except ValueError:
+            choice == 0
+            print("Option Not Valid")
     return choice
 
-def MoveFrog(GameList):
+def MoveFrog(GameList):#Gets movement info, checks if valid, then moves animal
     n1 = int(input("Select frog to move: "))
     n1 -=1 ##Creates index position from user input
     n2 = int(input("Select position to move to: "))
@@ -105,18 +113,18 @@ def CheckValidMove(n1, n2, GameList): ##Checks Reasons that move could not be co
 
 def AnyMovesValid(GameList):##Checks to see if there are any valid moves on the board
     ValidMove = False
-    for i in range((len(GameList))):
-        if(GameList[i] == "F"):
-            try:
+    for i in range((len(GameList))): ##For each item in list
+        if(GameList[i] == "F"): ##If Item is a frog
+            try: ##Tries for next two spaces
                 if(GameList[i-1] == "" or GameList[i-2] == ""):
                     ValidMove = True
-            except IndexError:
-                try:
+            except IndexError: ##Will throw if either index isn't valid
+                try: ##Tries again with the higher index
                     if(GameList[i-1] == ""):
                         ValidMove = True
-                except IndexError:
+                except IndexError: ##If higher index isn't valid
                     pass
-        if(GameList[i] == "T"):
+        if(GameList[i] == "T"): ##Does the same process as above but for toads
             try:
                 if(GameList[i+1] == "" or GameList[i+2] == ""):
                     ValidMove = True
@@ -126,9 +134,9 @@ def AnyMovesValid(GameList):##Checks to see if there are any valid moves on the 
                         ValidMove = True
                 except IndexError:
                     pass
-    return ValidMove 
+    return ValidMove ##returns true if any moves are valid
     
-def MoveFrogPositions(OriginalPos, NewPos, GameList):
+def MoveFrogPositions(OriginalPos, NewPos, GameList):#Moves frog within list
     GameList[NewPos] = GameList[OriginalPos]
     GameList[OriginalPos] = ""
     return GameList
@@ -148,6 +156,14 @@ def RunDemonstration():
     print("\n\tOnce they have switched places, the game has been won, like so")
     print("""["F", "F", "F", "", "T", "T", "T"] """)
     print("\n\tGood Luck!")
+
+def PrintRules():
+    print("""\tRules
+    - Frogs may only move right
+    - Toads may only move left
+    - Both frogs and toads can jump over one space to reach a free space
+    - Once both species have switched ends, the game is over
+    - The game can be reset at any time""")
 
 
 main()
